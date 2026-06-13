15204f0d1897187ddaefff8b5ebe44bc6afffbbf
leoji <41357275+LEO156@users.noreply.github.com>
2026-06-13 10:02:37 +0800

    Show argue submission modal per question

--- assets/js/config.public.js
+++ assets/js/config.public.js
@@ -1,7 +1,7 @@
 window.MRCAT_CONFIG = Object.freeze({
     cloudbaseEnvId: 'mrcat-dev-d9gwy2v1icdfdf597',
     region: 'ap-shanghai',
-    appVersion: '20260613-7',
+    appVersion: '20260613-8',
     defaultPassingPercentage: 50,
     defaultFeedbackPolicy: 'always',
     doneRangeDays: 7
